ccc6edf3d38c23133fe4ce886ff781755765f2a9
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-02-09 21:57:28 -0800

    Get rid of :return_response flag; it's not used

--- lib/appnexusapi/services/log_level_data_service.rb
+++ lib/appnexusapi/services/log_level_data_service.rb
@@ -8,11 +8,11 @@ class AppnexusApi::LogLevelDataService < AppnexusApi::ReadOnlyService
     params = {}
     params[:siphon_name] = @siphon_name if @siphon_name
     params[:updated_since] = time.strftime('%Y_%m_%d_%H') if time
-
-    # Anything with the same name and hour but with a newer timestamp is a republished replacement for an older file
-    # When this happens appnexus is supposed to set the checksum for the old file to null but they do not always
-    # actually do this.
     siphons = get(params)
+
+    # Anything with the same name and hour but with a newer timestamp is a republished replacement for an older file.
+    # When this happens appnexus is supposed to set the checksum for the old file to NULL but they do not always
+    # actually do this, so we have to manually reject invalid entries.
     siphons.reject do |siphon|
       (siphons - [siphon]).any? { |s| s.name == siphon.name && s.hour == siphon.hour && s.timestamp > siphon.timestamp }
     end
