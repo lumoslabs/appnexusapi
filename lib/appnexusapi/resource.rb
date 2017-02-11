28518aaf17b4387919adda0ce9444a5fa4240c1b
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-02-10 19:47:51 -0800

    replace raw_json method

--- lib/appnexusapi/resource.rb
+++ lib/appnexusapi/resource.rb
@@ -1,16 +1,16 @@
 class AppnexusApi::Resource
 
-  attr_reader :dbg_info, :json
+  attr_reader :dbg_info, :raw_json
 
   def initialize(json, service, dbg_info = nil)
-    @json = json
+    @raw_json = json
     @service = service
     @dbg_info = dbg_info
   end
 
   def update(route_params={}, body_params={})
     resource = @service.update(id, route_params, body_params)
-    @json = resource.json
+    @raw_json = resource.raw_json
     self
   end
 
@@ -19,20 +19,20 @@ class AppnexusApi::Resource
   end
 
   def method_missing(sym, *args, &block)
-    if @json.respond_to?(sym)
-      @json.send(sym, *args, &block)
-    elsif @json.has_key?(sym.to_s)
-      return @json[sym.to_s]
+    if @raw_json.respond_to?(sym)
+      @raw_json.send(sym, *args, &block)
+    elsif @raw_json.has_key?(sym.to_s)
+      return @raw_json[sym.to_s]
     else
       super(sym, *args, &block)
     end
   end
 
   def respond_to_missing?(method_name, include_private = false)
-    @json.respond_to?(method_name) || @json.has_key?(method_name.to_s) || super
+    @raw_json.respond_to?(method_name) || @raw_json.has_key?(method_name.to_s) || super
   end
 
   def to_s
-    @json.inspect
+    @raw_json.inspect
   end
 end
